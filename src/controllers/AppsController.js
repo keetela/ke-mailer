88954244a0fb41c6ff55cbd2528e827a2b96ce23
Ntwali-Josue <josh1234king@gmail.com>
2020-12-15 08:34:50 +0200

    add update and delete endpoint

--- src/controllers/AppsController.js
+++ src/controllers/AppsController.js
@@ -28,18 +28,98 @@ export const createApp = async (req, res) => {
 };
 
 export const fetchApps = async (req, res) => {
-  const apps = await Apps.findAll();
+  try {
+    const apps = await Apps.findAll();
 
-  if (apps.length < 1) {
-    return res.status(403).json({
-      status: 403,
-      message: 'There are no apps registered in the system',
+    if (apps.length < 1) {
+      return res.status(403).json({
+        status: 403,
+        message: 'There are no apps registered in the system',
+      });
+    }
+
+    return res.status(200).json({
+      status: 200,
+      message: 'Apps retrieved successfully',
+      Apps: apps,
+    });
+  } catch (error) {
+    return res.status(409).json({
+      status: 409,
+      message: error.message,
     });
   }
+};
 
-  return res.status(200).json({
-    status: 200,
-    message: 'Apps retrieved successfully',
-    Apps: apps,
-  });
+export const updateApps = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const existingApp = await Apps.findOne({
+      where: { id },
+    });
+
+    if (!existingApp) {
+      return res.status(200).json({
+        status: 200,
+        message: 'The App does not exist in the system',
+      });
+    }
+    const {
+      name, senderEmail, logo,
+    } = req.body;
+
+    const updateApp = await Apps.update({
+      name, senderEmail, logo,
+    },
+    {
+      where: { id: req.params.id }
+    });
+
+    if (updateApp) {
+      const updatedApp = await Apps.findOne({ where: { id: req.params.id } });
+      return res.status(200).json({
+        status: 200,
+        message: 'App updated successfully',
+        App: updatedApp
+      });
+    }
+    return res.status(400).json({
+      status: 400,
+      message: 'App not updated.'
+    });
+  } catch (error) {
+    return res.status(409).json({
+      message: error.message,
+    });
+  }
+};
+
+export const deleteApp = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const existingApp = await Apps.findOne({
+      where: { id },
+    });
+    if (!existingApp) {
+      return res.status(403).json({
+        status: 403,
+        message: 'App does not exist in the system'
+      });
+    }
+    const deleteById = await Apps.destroy({
+      where: { id },
+    });
+
+    if (deleteById) {
+      return res.status(200).json({
+        status: 200,
+        message: 'App deleted successfully.'
+      });
+    }
+  } catch (error) {
+    return res.status(409).json({
+      message: error.message,
+    });
+  }
 };
--- src/routes/api/apps.js
+++ src/routes/api/apps.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import { createApp, fetchApps } from '../../controllers/AppsController';
+import {
+  createApp, fetchApps, updateApps, deleteApp
+} from '../../controllers/AppsController';
 import asyncHandler from '../../middlewares/asyncHandler';
 import validateApp from '../../middlewares/validateApps';
 
@@ -7,5 +9,7 @@ const Apps = Router();
 
 Apps.get('/', asyncHandler(fetchApps));
 Apps.post('/', validateApp, asyncHandler(createApp));
+Apps.patch('/', validateApp, asyncHandler(updateApps));
+Apps.delete('/', validateApp, asyncHandler(deleteApp));
 
 export default Apps;
